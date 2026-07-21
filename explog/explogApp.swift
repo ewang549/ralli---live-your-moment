efd7a98b456c99b13b62258a037fa0b84146c5ff
ewang549 <ethan.rm.wang@gmail.com>
2026-07-21 15:44:52 -0700

    Initial Commit

---
+++ explog/explogApp.swift
@@ -0,0 +1,32 @@
+//
+//  explogApp.swift
+//  explog
+//
+//  Created by Ethan on 7/21/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct explogApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
